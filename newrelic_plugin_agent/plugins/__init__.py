2e0893b491c99706abe0842a57daef8edf03bb97
chunyang liu <cyliu7@gmail.com>
2015-05-21 17:19:22 +0800

    add host plugin to polling host metrics like cpu, mem, disk, network

--- newrelic_plugin_agent/plugins/__init__.py
+++ newrelic_plugin_agent/plugins/__init__.py
@@ -20,4 +20,5 @@ available = {
     'rabbitmq': 'newrelic_plugin_agent.plugins.rabbitmq.RabbitMQ',
     'redis': 'newrelic_plugin_agent.plugins.redis.Redis',
     'riak': 'newrelic_plugin_agent.plugins.riak.Riak',
-    'uwsgi': 'newrelic_plugin_agent.plugins.uwsgi.uWSGI'}
+    'uwsgi': 'newrelic_plugin_agent.plugins.uwsgi.uWSGI',
+    'host': 'newrelic_plugin_agent.plugins.host.Host'}
